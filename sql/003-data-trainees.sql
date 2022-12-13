81041823a6ef38a51749f9d023d70febf5afb660
Maxime Balan <maxime.balan@gmail.com>
2022-12-13 09:16:49 +0100

    SQL

---
+++ sql/003-data-trainees.sql
@@ -0,0 +1,23 @@
+INSERT INTO trainees (lastname, firstname,birthdate,email, poe_id)
+    VALUES ('Holmes','Sherlock','1854-06-12','sherlock.holmes@baker.uk',1);
+INSERT INTO trainees (lastname, firstname,birthdate,email, poe_id)
+    VALUES ('Moriarty','James','1854-04-01','james.moriarty@london.uk',1);
+INSERT INTO trainees (lastname, firstname,birthdate,email, poe_id,2)
+    VALUES ('Bond','James','1967-05-23','james.bond@mi6.org',3);
+INSERT INTO trainees (lastname, firstname,birthdate,email, poe_id)
+    VALUES ('Holmes','Mycroft','1852-08-05','mycroft.holmes@bp.uk',3);
+INSERT INTO trainees (lastname, firstname,birthdate,email, poe_id)
+    VALUES ('Baggins','Frodo','325-09-22','frodo@shire.me',4);
+INSERT INTO trainees (lastname, firstname, birthdate, email, poe_id)
+    VALUES ('Green', 'Rachel', '1972-11-06', 'rachel.green@gmail.com',2);
+INSERT INTO trainees (lastname, firstname, birthdate, email, gender, phone_number, poe_id)
+    VALUES ('Captain', 'Marvel', '1969-05-31', 'captain@marvel.org', 'F', '+330102030407',6);
+INSERT INTO trainees (lastname, firstname, birthdate, email, poe_id)
+    VALUES ('Miaou', 'Spatule', '2000-11-06', 'jaimelacroquette@miaou.fr',4);
+INSERT INTO trainees (lastname, firstname, birthdate, email, poe_id)
+    VALUES ('Africa', 'Toto', '1975-05-11', 'toto.africa@music.fr',5);
+INSERT INTO trainees (lastname, firstname, birthdate, email, gender, phone_number, poe_id)
+    VALUES ('Poppins', 'Mary', '1965-11-25', 'mary.poppins@disney.fr', 'F', '+33102030405',4);
+INSERT INTO trainees (lastname, firstname, birthdate, email, gender, phone_number, poe_id)
+    VALUES ('Pedestre', 'Petit', '1974-03-21', 'petit@pedestre.org', 'M', '+330102030414',6);
+
